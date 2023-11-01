9f1893f60d56993c81ee0c35cc085a0421b2c6f9
elchinovzla <sonnyng.vzla@gmail.com>
2023-11-01 15:10:13 -0600

    create a bubble sort

---
+++ algorithms/sorting/bubbleSort.py
@@ -0,0 +1,13 @@
+def bubbleSort(numbers: [int]):
+    collectionLength = len(numbers)
+    for i in range(0, collectionLength):
+        for j in range(0, collectionLength-i-1):
+            if numbers[j] > numbers[j+1]:
+                temp = numbers[j]
+                numbers[j] = numbers[j+1]
+                numbers[j+1] = temp
+    return numbers
+
+
+print(bubbleSort([1, 3, 2, 5, 23, 1, 5, 21, 50, 60,
+      0, 12, 435, 123, 12, 12, 34, 23, 12, 7, 5, 7]))
