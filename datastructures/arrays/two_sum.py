4e8e1f399efe8b07a43f0c58606c82513883c395
elchinovzla <sonnyng.vzla@gmail.com>
2023-11-07 16:18:18 -0700

    improve two_sum tests

--- datastructures/arrays/two_sum.py
+++ datastructures/arrays/two_sum.py
@@ -1,26 +1,44 @@
 # https://leetcode.com/problems/two-sum/description/
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+import unittest
 
-        number_needed_for_target_dict = {}
 
-        if len(nums) < 2:
-            return []
+def two_sum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
 
-        for index, number in enumerate(nums):
-            number_needed_for_target = target - number
-            if number_needed_for_target in number_needed_for_target_dict:
-                return [number_needed_for_target_dict[number_needed_for_target], index]
-            number_needed_for_target_dict[number] = index
+    target_indexes = {}
 
+    if len(nums) < 2:
         return []
 
+    for index, number in enumerate(nums):
+        number_needed_for_target = target - number
+        if number_needed_for_target in target_indexes:
+            return [target_indexes[number_needed_for_target], index]
+        target_indexes[number] = index
 
-solution = Solution()
-print(solution.twoSum([2, 7, 11, 15], 9))
+    return []
+
+
+class Tests(unittest.TestCase):
+    def test_twosum_answerwithinfirstindexes_returnsfirstindexes(self):
+        self.assertEqual(two_sum([2, 7, 11, 15], 9), [0, 1])
+
+    def test_twosum_answerwithinlast_indexes_returnslastindexes(self):
+        self.assertEqual(two_sum([1, 3, 7, 9, 2], 11), [3, 4])
+
+    def test_twosum_emptyarray_returnsempty(self):
+        self.assertEqual(two_sum([], 5), [])
+
+    def test_twosum_arraytoosmall_returnsempty(self):
+        self.assertEqual(two_sum([5], 11), [])
+
+    def test_twosum_arraydonothavevalues_returnsempty(self):
+        self.assertEqual(two_sum([1, 2, 5, 4], 19), [])
+
+
+unittest.main(exit=False)
