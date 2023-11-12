734a1f7b0b4d0846ee541cdcea17be5657ca0ee0
elchinovzla <sonnyng.vzla@gmail.com>
2023-11-11 21:22:55 -0700

    fix test names

--- datastructures/strings/leetcode/valid_palidrome.py
+++ datastructures/strings/leetcode/valid_palidrome.py
@@ -3,10 +3,9 @@ import unittest
 
 
 class Solution(object):
-    def isPalidrome(self, s):
+    def validPalindrome(self, s):
         """
         :type s: str
-        :type t: str
         :rtype: bool
         """
         cleaned_sentence = "".join(filter(str.isalnum, str(s).lower()))
@@ -23,32 +22,32 @@ class Solution(object):
 class Tests(unittest.TestCase):
     def test_lengthOfLongestSubstring_example1(self):
         solution = Solution()
-        self.assertTrue(solution.isPalidrome(
+        self.assertTrue(solution.validPalindrome(
             "aabaa"))
 
-    def test_lengthOfLongestSubstring_example1(self):
+    def test_lengthOfLongestSubstring_example2(self):
         solution = Solution()
-        self.assertTrue(solution.isPalidrome(
+        self.assertTrue(solution.validPalindrome(
             "aabbaa"))
 
-    def test_lengthOfLongestSubstring_example1(self):
+    def test_lengthOfLongestSubstring_example3(self):
         solution = Solution()
-        self.assertFalse(solution.isPalidrome(
+        self.assertFalse(solution.validPalindrome(
             "abc"))
 
-    def test_lengthOfLongestSubstring_example1(self):
+    def test_lengthOfLongestSubstring_example4(self):
         solution = Solution()
-        self.assertTrue(solution.isPalidrome(
+        self.assertTrue(solution.validPalindrome(
             "a"))
 
-    def test_lengthOfLongestSubstring_example1(self):
+    def test_lengthOfLongestSubstring_example5(self):
         solution = Solution()
-        self.assertTrue(solution.isPalidrome(
+        self.assertTrue(solution.validPalindrome(
             ""))
 
-    def test_lengthOfLongestSubstring_example1(self):
+    def test_lengthOfLongestSubstring_example6(self):
         solution = Solution()
-        self.assertTrue(solution.isPalidrome(
+        self.assertTrue(solution.validPalindrome(
             "A man, a plan, a canal: Panama"))
 
 
