8f7a9c88396ac487adbbb5d0415894e9f090528b
elchinovzla <sonnyng.vzla@gmail.com>
2023-11-14 15:13:53 -0700

    solve q. 20

---
+++ datastructures/stacks/leetcode/valid_parentheses.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/valid-parentheses/description/
+
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        brackets = {
+            "[": "]",
+            "(": ")",
+            "{": "}"
+        }
+        open_brackets = list()
+
+        for char in s:
+            if char in brackets.keys():
+                open_brackets.append(char)
+            elif char in brackets.values():
+                if not open_brackets or brackets[open_brackets.pop()] != char:
+                    return False
+        return not open_brackets
