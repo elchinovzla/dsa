4184676afa913421f8765c1f4a5102436d931500
elchinovzla <sonnyng.vzla@gmail.com>
2023-11-16 10:43:35 -0700

    solve q 102

---
+++ datastructures/trees/leetcode/binary_tree_level_order_traversal.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        queue = [root]
+        answer = list()
+        while queue:
+            sub_array = []
+            for _ in range(len(queue)):
+                current_node = queue.pop(0)
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+
+                sub_array.append(current_node.val)
+            answer.append(sub_array)
+
+        return answer
