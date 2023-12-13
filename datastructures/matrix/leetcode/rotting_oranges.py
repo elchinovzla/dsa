1037ac849f846aa5fdd2cd3adff5e3dae290d97c
elchinovzla <sonnyng.vzla@gmail.com>
2023-12-13 16:19:59 -0700

    solve q.994 following udemy's design

--- datastructures/matrix/leetcode/rotting_oranges.py
+++ datastructures/matrix/leetcode/rotting_oranges.py
@@ -9,62 +9,42 @@ class Solution(object):
         row_length = len(grid)
         col_length = len(grid[0])
         timer_counter = 0
+        fresh_oranges_counter = 0
+        rotten_oranges = []
 
-        def check_oranges():
-            oranges = {
-                "rotten": [],
-                "fresh": []
-            }
+        for row in range(row_length):
+            for col in range(col_length):
+                if grid[row][col] == 2:
+                    rotten_oranges.append([row, col])
+                elif grid[row][col] == 1:
+                    fresh_oranges_counter += 1
 
-            for row in range(row_length):
-                for col in range(col_length):
-                    if grid[row][col] == 2:
-                        oranges["rotten"].append([row, col])
-                    elif grid[row][col] == 1:
-                        oranges["fresh"].append([row, col])
+        queue_size = len(rotten_oranges)
+        while len(rotten_oranges) > 0:
+            current_rotten_orange = rotten_oranges.pop(0)
+            queue_size -= 1
+            row = current_rotten_orange[0]
+            col = current_rotten_orange[1]
 
-            return oranges
-
-        def rot_nearby_oranges(row, col):
-            has_an_orange_rot = False
             if row - 1 >= 0 and grid[row-1][col] == 1:
                 grid[row-1][col] = 2
-                has_an_orange_rot = True
+                rotten_oranges.append([row-1, col])
+                fresh_oranges_counter -= 1
             if col + 1 < col_length and grid[row][col + 1] == 1:
                 grid[row][col+1] = 2
-                has_an_orange_rot = True
+                rotten_oranges.append([row, col+1])
+                fresh_oranges_counter -= 1
             if row + 1 < row_length and grid[row+1][col] == 1:
                 grid[row+1][col] = 2
-                has_an_orange_rot = True
+                rotten_oranges.append([row+1, col])
+                fresh_oranges_counter -= 1
             if col - 1 >= 0 and grid[row][col-1] == 1:
                 grid[row][col-1] = 2
-                has_an_orange_rot = True
+                rotten_oranges.append([row, col-1])
+                fresh_oranges_counter -= 1
 
-            return has_an_orange_rot
+            if queue_size == 0 and len(rotten_oranges) > 0:
+                timer_counter += 1
+                queue_size = len(rotten_oranges)
 
-        def check_rotten_oranges(rotten_oranges):
-            has_an_orange_rot = False
-
-            while rotten_oranges:
-                current_position = rotten_oranges.pop()
-                current_row = current_position[0]
-                current_col = current_position[1]
-                grid[current_row][current_col] = -1
-                has_a_new_orange_rot = rot_nearby_oranges(
-                    current_row, current_col)
-                has_an_orange_rot = has_an_orange_rot or has_a_new_orange_rot
-
-            return has_an_orange_rot
-
-        oranges = check_oranges()
-        rotten_oranges = oranges.get("rotten")
-        fresh_oranges = oranges.get("fresh")
-
-        while rotten_oranges:
-            were_new_oranges_rot = check_rotten_oranges(rotten_oranges)
-            oranges = check_oranges()
-            rotten_oranges = oranges.get("rotten")
-            fresh_oranges = oranges.get("fresh")
-            timer_counter += 1 if were_new_oranges_rot else 0
-
-        return -1 if len(fresh_oranges) > 0 else timer_counter
+        return -1 if fresh_oranges_counter > 0 else timer_counter
